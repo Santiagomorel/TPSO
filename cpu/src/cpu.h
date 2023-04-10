f2cd1ebc1781a97f51d7cacf63cdef19fef23d2c
guidogelvan <ggelvan@frba.utn.edu.ar>
2023-04-10 23:52:51 +0000

    Subo cliente sin el servidor de CPU y commiteo el cpu.h

---
+++ cpu/src/cpu.h
@@ -0,0 +1,24 @@
+#ifndef CPU_H_
+#define CPU_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+//#include <cspecs/cspec.h>
+
+
+#include<utils/utils_client.h>
+
+
+t_log* iniciar_logger(void);
+t_config* iniciar_config(void);
+void leer_consola(t_log*);
+void paquete(int);
+void terminar_programa(int, t_log*, t_config*);
+
+
+
+#endif 
