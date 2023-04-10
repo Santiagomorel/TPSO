3b1b495bff80ca2e54296b6c56625026303aa8be
guidogelvan <ggelvan@frba.utn.edu.ar>
2023-04-10 22:53:54 +0000

    CPU

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -3,22 +3,189 @@
 #include <commons/config.h>
 #include <utils/hello.h>
 #include <valgrind/valgrind.h>
+#include <commons/log.h>
 
 void funcion(char *str, int i) {
     VALGRIND_PRINTF_BACKTRACE("%s: %d\n", str, i);
 }
 
 int main() {
-    funcion("Hola Mundo", 42);
-    //agrego data
-    //mas data.
-    return 0;
+    int conexion_cpu;
+	char* ip_memoria;
+	char* puerto_memoria;
+	char* retardo_instruccion;
+	char* puerto_escucha;
+	char* tam_max_segmento;
+
+	t_log* logger;
+	t_config* config;
+
+    /* ---------------- LOGGING ---------------- */
+
+	logger = iniciar_logger();
+
+
+	// Usando el logger creado previamente
+	// Escribi: "Hola! Soy un log"
+
+	log_info(logger, "Hola! Soy un Log");
+
+	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
+
+	config = iniciar_config();
+
+	// Usando el config creado previamente, leemos los valores del config y los 
+	// dejamos en las variables 'ip', 'puerto' y 'valor'
+
+	ip_memoria = config_get_string_value(config, "IP_MEMORIA");
+	puerto_memoria = config_get_string_value(config, "PUERTO_MEMORIA");
+	retardo_instruccion = config_get_string_value(config, "RETARDO_INSTRUCCION");
+	puerto_escucha = config_get_string_value(config, "PUERTO_ESCUCHA");
+	tam_max_segmento = config_get_string_value(config, "TAM_MAX_SEGMENTO");
+
+	
+
+	log_info(logger,"Lei la IP %s , el Puerto Memoria %s ", ip_memoria, puerto_memoria);
+
+
+	// Loggeamos el valor de config
+
+    /* ---------------- LEER DE CONSOLA ---------------- */
+
+	leer_consola(logger);
+
+	/*----------------------------------------------------------------------------------------------------------------*/
+
+
+	// Creamos una conexión hacia el servidor
+	conexion_cpu = crear_conexion(ip_memoria, puerto_memoria);
+
+	// Enviamos al servidor el valor de ip como mensaje
+	enviar_mensaje(ip_memoria, conexion_cpu);
+
+	// Armamos y enviamos el paquete
+	paquete(conexion_cpu);
+
+	terminar_programa(conexion_cpu, logger, config);
+
+//----------------------------Inicio a la CPU como servidor ------------------------------------
+	
+    logger = log_create("log.log", "Servidor", 1, LOG_LEVEL_TRACE);
+
+	int kernel_cpu = iniciar_servidor();
+	log_info(logger, "Servidor listo para recibir al cliente");
+	int cliente_fd = esperar_cliente(kernel_cpu);
+
+	t_list* lista;
+	while (1) {
+		int cod_op = recibir_operacion(cliente_fd);
+		switch (cod_op) {
+		case MENSAJE:
+			recibir_mensaje(cliente_fd);
+			break;
+		case PAQUETE:
+			lista = recibir_paquete(cliente_fd);
+			log_info(logger, "Me llegaron los siguientes valores:\n");
+			list_iterate(lista, (void*) iterator);
+			break;
+		case -1:
+			log_error(logger, "el cliente se desconecto. Terminando servidor");
+			return EXIT_FAILURE;
+		default:
+			log_warning(logger,"Operacion desconocida. No quieras meter la pata");
+			break;
+		}
+	}
+	return EXIT_SUCCESS;
 }
 
-/*int main(void) {
-  hello_world();
-  return 0;
-  }*/
+void iterator(char* value) {
+	log_info(logger,"%s", value);
+}
+
+
+
+t_log* iniciar_logger(void)
+{
+	t_log* nuevo_logger;
+
+	if ((nuevo_logger = log_create("tp0.log", "Logs tp0",1 , LOG_LEVEL_INFO)) == NULL){
+		printf("No se pudo crear el logger \n");
+		exit(1);
+	}
+
+	return nuevo_logger;
+}
+
+t_config* iniciar_config(void)
+{
+	t_config* nuevo_config;
+
+	if ((nuevo_config = config_create("/home/utnso/Desktop/TP 2023/tp-2023-1c-EAB-MODEL/cpu/config/cpu.config")) == NULL){
+		printf("No se pudo leer la config");
+		exit(2);
+	}
+
+	return nuevo_config;
+}
+
+void leer_consola(t_log* logger)
+{
+	
+    char* leido;
+
+    // La primera te la dejo de yapa
+    leido = readline("> ");
+
+    // El resto, las vamos leyendo y logueando hasta recibir un string vacío
+    while(strcmp(leido, "")) {
+        log_info(logger, leido);
+        leido = readline("> "); 
+    }
+	free(leido); 
+}
+
+void paquete(int conexion)
+{
+	// Ahora toca lo divertido!
+	t_paquete* paquete = crear_paquete();
+	char* leido = readline("> ");
+
+	// Leemos y esta vez agregamos las lineas al paquete
+	while(strcmp(leido, "")) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);
+		free(leido);
+		leido = readline("> ");
+	}
+	enviar_paquete(paquete, conexion);
+
+	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
+
+	eliminar_paquete(paquete);
+	free(leido);
+	
+}
+
+void terminar_programa(int conexion, t_log* logger, t_config* config)
+{
+	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
+	  con las funciones de las commons y del TP mencionadas en el enunciado */
+
+	if(logger != NULL){
+		log_destroy(logger);
+	}
+
+	if(config != NULL){
+		config_destroy(config);
+	}
+
+	liberar_conexion(conexion);
+}
+
+
+
+
+
 /*int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Se esperaba: %s [CONFIG_PATH]\n", argv[0]);
