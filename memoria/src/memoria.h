2318ac21a7716220ea9a9fc4249bbd2723afa31e
Gonzalo Garmon <81934183+GonzaloGarmon@users.noreply.github.com>
2023-04-15 10:35:03 -0300

    Update memoria.h
    struct memoria_config

--- memoria/src/memoria.h
+++ memoria/src/memoria.h
@@ -2,7 +2,19 @@
 #define MEMORIA_H_
 
 #include <utils/utils_server.h>
+typedef struct{
 
+    char* puerto_escucha;
+    char* tam_memoria;
+    char* tam_segmento;
+    char* cant_segmentos;
+    char* retardo_memoria;
+    char* retardo_compactacion;
+    char* algoritmo_asignacion;
+
+} Memoria_config;
+
+Memoria_config kernel_config;
 #endif /*MEMORIA_H_*/
 
 
