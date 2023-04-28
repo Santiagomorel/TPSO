2737d2ce36d64ee6f45aa65a2a844406ae3806c1
guidogelvan <ggelvan@frba.utn.edu.ar>
2023-04-28 02:07:16 +0000

    Creo pcb y registros con algunas funciones

--- cpu/src/cpu.c
+++ cpu/src/cpu.c
@@ -12,15 +12,14 @@ void funcion(char *str, int i) {
 
 
 int main(int argc, char ** argv) {
-    int conexion_cpu;
-	int socket_cpu;
+
 
 
     /* ---------------- LOGGING ---------------- */
 
 	cpu_logger = init_logger("./runlogs/cpu.log", "CPU", 1, LOG_LEVEL_TRACE);
 
-	    log_info(cpu_logger, "levanto la configuracion del cpu");
+	    log_info(cpu_logger, "Levanto la configuracion del cpu");
     if (argc < 2) {
         fprintf(stderr, "Se esperaba: %s [CONFIG_PATH]\n", argv[0]);
         exit(1);
@@ -34,19 +33,21 @@ int main(int argc, char ** argv) {
         exit(1);
     }
 
-    log_info(cpu_logger, "cargo la configuracion de la cpu");
+    log_info(cpu_logger, "Cargo la configuracion de la cpu");
     
     load_config();
 	
-//-----------------------------------------------------------------------
+/*--------------------- CONECTO CON MEMORIA ----------------------*/
 
-	establecer_conexion(cpu_config.ip_memoria, cpu_config.puerto_memoria, conexion_cpu, cpu_config_file, cpu_logger);
+	establecer_conexion(cpu_config.ip_memoria, cpu_config.puerto_memoria, cpu_config_file, cpu_logger);
+
+/*---------------------- CONEXION CON KERNEL ---------------------*/
 
 	socket_cpu = iniciar_servidor(cpu_config.puerto_escucha, cpu_logger);
 	esperar_cliente(socket_cpu);
 	handshake_servidor(socket_cpu);
 
-
+/*---------------------- TERMINO CPU ---------------------*/
 	terminar_programa(conexion_cpu, cpu_logger, cpu_config_file);
 
 	return 0;
@@ -60,12 +61,12 @@ void load_config(void){
 	cpu_config.retardo_instruccion					= config_get_string_value(cpu_config_file, "RETARDO_INSTRUCCION");
 	cpu_config.tam_max_segmento						= config_get_string_value(cpu_config_file, "TAM_MAX_SEGMENTO");
 
-	log_info(cpu_logger, "config cargada en 'cpu_cofig_file'");
+	log_info(cpu_logger, "Config cargada en 'cpu_cofig_file'");
 
 
 }
 
-void establecer_conexion(char * ip_memoria, char* puerto_memoria, int conexion_cpu, t_config* config, t_log* logger){
+void establecer_conexion(char * ip_memoria, char* puerto_memoria, t_config* config, t_log* logger){
 
 	
 	log_info(logger, "Inicio como cliente");
@@ -114,7 +115,11 @@ void handshake_cliente(int socket_cliente){
 	uint32_t result;
 
 	send(socket_cliente, &handshake, sizeof(uint32_t), NULL);
-	recv(socket_cliente, &result, sizeof(uint32_t), MSG_WAITALL);
+	if(recv(socket_cliente, &result, sizeof(uint32_t), MSG_WAITALL) > 0){
+		log_info(cpu_logger, "Establecí handshake con Memoria");
+	}else{
+		log_info(cpu_logger, "No pude establecer handshake con Memoria");
+	}
 }
 
 
@@ -149,6 +154,177 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	liberar_conexion(conexion);
 }
 
+/*-------------------- REGISTROS -------------------*/
+void set_registers(t_pcb* pcb) {
+    registers[AX] = pcb->cpu_registers[AX];
+    registers[BX] = pcb->cpu_registers[BX];
+    registers[CX] = pcb->cpu_registers[CX];
+    registers[DX] = pcb->cpu_registers[DX];
+	registers[EAX] = pcb->cpu_registers[EAX];
+	registers[EBX] = pcb->cpu_registers[EBX];
+	registers[ECX] = pcb->cpu_registers[ECX];
+	registers[EDX] = pcb->cpu_registers[EDX];
+	registers[RAX] = pcb->cpu_registers[RAX];
+	registers[RBX] = pcb->cpu_registers[RBX];
+	registers[RCX] = pcb->cpu_registers[RCX];
+	registers[RDX] = pcb->cpu_registers[RDX];
+
+
+}
+
+void init_registers() {
+    registers[AX] = 0;
+    registers[BX] = 0;
+    registers[CX] = 0;
+    registers[DX] = 0;
+	registers[EAX] = 0;
+    registers[EBX] = 0;
+    registers[ECX] = 0;
+    registers[EDX] = 0;
+	registers[RAX] = 0;
+    registers[RBX] = 0;
+    registers[RCX] = 0;
+    registers[RDX] = 0;
+}
+
+/*------------------- BUFFER --------------------*/
+void* receive_buffer(int* size, int client_socket){
+	void* buffer;
+	recv(client_socket, size, sizeof(int), MSG_WAITALL);
+	buffer = malloc(*size);
+	recv(client_socket, buffer, *size, MSG_WAITALL);
+	return buffer;
+}
+
+/* ---------------- PCB ----------------*/
+
+t_pcb* pcb_create(char** instructions, int client_socket, int pid) { // Para crear el PCB
+    t_pcb* new_pcb = malloc(sizeof(t_pcb));
+
+    new_pcb->process_id = pid;
+    new_pcb->size = 0; // TODO: Ver en que basarse para ponele un size
+    new_pcb->status = NEW;
+	new_pcb->instructions = instructions;
+    new_pcb->program_counter = 0;
+    new_pcb->client_socket = client_socket;
+
+    new_pcb->cpu_registers[AX] = 0;
+	new_pcb->cpu_registers[BX] = 0;
+	new_pcb->cpu_registers[CX] = 0;
+	new_pcb->cpu_registers[DX] = 0;
+	new_pcb->cpu_registers[EAX] = 0;
+	new_pcb->cpu_registers[EBX] = 0;
+	new_pcb->cpu_registers[ECX] = 0;
+	new_pcb->cpu_registers[EDX] = 0;
+	new_pcb->cpu_registers[RAX] = 0;
+	new_pcb->cpu_registers[RBX] = 0;
+	new_pcb->cpu_registers[RCX] = 0;
+	new_pcb->cpu_registers[RDX] = 0;
+ 
+
+    return new_pcb;
+}
+void save_context_pcb(t_pcb* pcb){
+    pcb->cpu_registers[AX] = registers[AX];
+    pcb->cpu_registers[BX] = registers[BX];
+    pcb->cpu_registers[CX] = registers[CX];
+    pcb->cpu_registers[DX] = registers[DX];
+	pcb->cpu_registers[EAX] = registers[EAX];
+	pcb->cpu_registers[EBX] = registers[EBX];
+	pcb->cpu_registers[ECX] = registers[ECX];
+	pcb->cpu_registers[EDX] = registers[EDX];
+	pcb->cpu_registers[RAX] = registers[RAX];
+	pcb->cpu_registers[RBX] = registers[RBX];
+	pcb->cpu_registers[RCX] = registers[RCX];
+	pcb->cpu_registers[RDX] = registers[RDX];
+
+}
+
+
+char* get_state_name(int state){
+	char* string_state;
+	switch(state){
+		case NEW :
+			string_state = string_duplicate("NEW");
+			break;
+		case READY:
+			string_state = string_duplicate("READY");
+			break;
+		case BLOCKED:
+			string_state = string_duplicate("BLOCKED");
+			break;
+		case RUNNING:
+			string_state = string_duplicate("RUNNING");
+			break;
+		case EXIT:
+			string_state = string_duplicate("EXIT");
+			break;
+		default:
+			string_state = string_duplicate("ESTADO NO REGISTRADO");
+			break;
+	}
+	return string_state;
+}
+
+void logguar_state(t_log* logger, int state) {
+	char* string_state = get_state_name(state);
+	log_info(logger, "Estado %d (%s)", state, string_state);
+	free(string_state);
+}
+
+
+int read_int(char* buffer, int* desp) {
+	int ret;
+	memcpy(&ret, buffer + (*desp), sizeof(int));
+	(*desp)+=sizeof(int);
+	return ret;
+}
+
+/*---------------------- PARA INSTRUCCION SET -------------------*/
+
+void add_value_to_register(char* registerToModify, char* valueToAdd){ 
+    //convertir el valor a agregar a un tipo de dato int
+    uint32_t value = atoi(valueToAdd);
+    
+    log_info(cpu_logger, "valor a sumarle al registro %d",value);
+    if (strcmp(registerToModify, "AX") == 0) {
+        registers[AX] += value;
+    }
+    else if (strcmp(registerToModify, "BX") == 0) {
+        registers[BX] += value;
+    }
+    else if (strcmp(registerToModify, "CX") == 0) {
+        registers[CX] += value;
+    }
+    else if (strcmp(registerToModify, "DX") == 0) {
+        registers[DX] += value;
+    }else if (strcmp(registerToModify, "EAX") == 0) {
+        registers[EAX] += value;
+    }else if (strcmp(registerToModify, "EBX") == 0) {
+        registers[EBX] += value;
+    }
+    else if (strcmp(registerToModify, "ECX") == 0) {
+        registers[ECX] += value;
+    }
+    else if (strcmp(registerToModify, "EDX") == 0) {
+        registers[EDX] += value;
+    }else if (strcmp(registerToModify, "RAX") == 0) {
+        registers[RAX] += value;
+    }else if (strcmp(registerToModify, "RBX") == 0) {
+        registers[RBX] += value;
+    }
+    else if (strcmp(registerToModify, "RCX") == 0) {
+        registers[RCX] += value;
+    }
+    else if (strcmp(registerToModify, "RDX") == 0) {
+        registers[RDX] += value;
+    }
+}
+
+/*-------------------- FETCH ---------------------- */
+char* fetch_next_instruction_to_execute(t_pcb* pcb){
+    return pcb->instructions[pcb->program_counter];
+}
 
 
 
--- cpu/src/cpu.h
+++ cpu/src/cpu.h
@@ -26,11 +26,79 @@ CPU_config cpu_config;
 t_log * cpu_logger;
 t_config * cpu_config_file;
 
+int conexion_cpu;
+int socket_cpu;
+
 
 
 void leer_consola(t_log*);
 void paquete(int);
 void terminar_programa(int, t_log*, t_config*);
-void establecer_conexion(char* , char* , int, t_config*, t_log*);
+void establecer_conexion(char* , char* , t_config*, t_log*);
+
+/*-------------- REGISTROS ------------*/
+char registers[4];
+char registers[4];
+char registers[4];
+
+#define AX 0
+#define BX 1
+#define CX 2
+#define DX 3
+#define EAX 4
+#define EBX 5
+#define ECX 6
+#define EDX 7
+#define RAX 8
+#define RBX 9
+#define RCX 10
+#define RDX 11
+
+void init_registers();
+void add_value_to_register(char* registerToModify, char* valueToAdd);
+void add_two_registers(char* registerToModify, char* registroParaSumarleAlOtroRegistro);
+
+
+/*------------------- BUFFER --------------------*/
+void* receive_buffer(int*, int);
+
+
+/*-------------- PCB -------------------*/
+
+typedef enum { // Los estados que puede tener un PCB
+    NEW,
+    READY,
+    BLOCKED,
+    RUNNING,
+    EXIT,
+} pcb_status;
+typedef struct {
+    int process_id; //process id identificador del proceso.
+    int size;
+    int program_counter; // número de la próxima instrucción a ejecutar.
+	int client_socket;
+    uint32_t cpu_registers[12]; //Estructura que contendrá los valores de los registros de uso general de la CPU. mas adelante hay que pensarla
+    char** instructions; // lista de instrucciones a ejecutar. char**
+	pcb_status status;
+} t_pcb;
+
+
+
+t_pcb* pcb_create(char** instructions, int client_socket, int pid);
+char** generate_instructions_list(char* instructions);
+
+char* get_state_name(int state);
+void logguar_state(t_log* logger, int state);
+
+t_pcb* receive_pcb(int socket, t_log* logger);
+
+void change_pcb_state_to(t_pcb* pcb, pcb_status newState);
+void loggear_pcb(t_pcb* pcb, t_log* logger);
+void loggear_registers(t_log* logger, uint32_t* registers);
+
+int read_int(char* buffer, int* desp);
+
+/*-------------- PROCESOS --------------------*/
+char* fetch_next_instruction_to_execute(t_pcb* pcb);
 
 #endif
